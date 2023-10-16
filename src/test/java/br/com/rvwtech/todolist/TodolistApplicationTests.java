2d7b70bc1645963018a4d94adc0b298a44568386
Rafa Wierzba <rafaelwierzba@gmail.com>
2023-10-16 14:13:57 -0300

    Add files via upload

---
+++ src/test/java/br/com/rvwtech/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rvwtech.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
